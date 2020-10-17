6f7897dbb78d6eae94384cbe8285fb824769ce40
GantGanz <53847702+GantGanz@users.noreply.github.com>
2020-10-17 19:42:56 +0700

    Perbaikan script.js 1

--- koperasi/script/script.js
+++ koperasi/script/script.js
@@ -1,22 +1,15 @@
-// $(document).ready(function () {
-//     $('#keyword').on('keyup', function () {
-//         $('#tabelJquery').load('../ajax/anggota.php?keyword=' + $('keyword').val());
-//     });
-// });
+var keyword = document.getElementById("keyword");
+var tombolCari = document.getElementById("tombolCari");
+var tabelPencarian = document.getElementById("tabelPencarian");
 
-var keyword = document.getElementById('keyword');
-var tombolCari = document.getElementById('tombolCari');
-var tabelPencarian = document.getElementById('tabelPencarian');
-
-keyword.addEventListener('keyup', function () {
+keyword.addEventListener("keyup", function () {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             tabelPencarian.innerHTML = xhr.responseText;
-            console.log('ok');
         }
-    }
+    };
 
-    xhr.open('GET', '../ajax/anggota.php?keyword=' + keyword.value, true);
+    xhr.open("GET", "../ajax/anggota.php?keyword=" + keyword.value, true);
     xhr.send();
-})
+});
